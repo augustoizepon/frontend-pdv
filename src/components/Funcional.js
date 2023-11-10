d5d55ec9f77ee757ce52bbadf0ffcc8efbb6b436
augustoizepon <augustom.izepon@gmail.com>
2023-11-10 18:02:39 -0300

    feat: melhor layout e melhor organização

--- src/components/Funcional.js
+++ src/components/Funcional.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../style/Funcional.css';
-import axios from 'axios';
+// import axios from 'axios';
 import ProfileUpdate from './ProfileUpdate';
 import CategoryList from './CategoryList';
 import { token } from './login';
@@ -9,15 +9,15 @@ import { token } from './login';
 export default function Funcional() {
     const [visibilityUpdateYourData, setVisibilityUpdateYourData] = useState(false);
     const [visibilityCatogryList, setVisibilityCatogryList] = useState(false);
-    const [visibilityCreateProduct, setVisibilityCreateProduct] = useState(false);
-    const [visibilityEditProduct, setVisibilityEditProduct] = useState(false);
-    const [visibilityListProducts, setVisibilityListProducts] = useState(false);
-    const [visibilityDetailProduct, setVisibilityDetailProduct] = useState(false);
-    const [visibilityDeleteProduct, setVisibilityDeleteProduct] = useState(false);
-    const [visibilityCreateCustomer, setVisibilityCreateCustomer] = useState(false);
-    const [visibilityEditCustomer, setVisibilityEditCustomer] = useState(false);
-    const [visibilityListCustomers, setVisibilityListCustomers] = useState(false);
-    const [visibilityDetailCustomer, setVisibilityDetailCustomer] = useState(false);
+    // const [visibilityCreateProduct, setVisibilityCreateProduct] = useState(false);
+    // const [visibilityEditProduct, setVisibilityEditProduct] = useState(false);
+    // const [visibilityListProducts, setVisibilityListProducts] = useState(false);
+    // const [visibilityDetailProduct, setVisibilityDetailProduct] = useState(false);
+    // const [visibilityDeleteProduct, setVisibilityDeleteProduct] = useState(false);
+    // const [visibilityCreateCustomer, setVisibilityCreateCustomer] = useState(false);
+    // const [visibilityEditCustomer, setVisibilityEditCustomer] = useState(false);
+    // const [visibilityListCustomers, setVisibilityListCustomers] = useState(false);
+    // const [visibilityDetailCustomer, setVisibilityDetailCustomer] = useState(false);
 
     const toggleVisibilityUpdateYourData = () => {
         setVisibilityUpdateYourData(true);
@@ -27,48 +27,48 @@ export default function Funcional() {
         setVisibilityCatogryList(true);
     };
 
-    const toggleVisibilityCreateProduct = () => {
-        setVisibilityCreateProduct(true);
-    };
+    // const toggleVisibilityCreateProduct = () => {
+    //     setVisibilityCreateProduct(true);
+    // };
 
-    const toggleVisibilityEditProduct = () => {
-        setVisibilityEditProduct(true);
-    };
+    // const toggleVisibilityEditProduct = () => {
+    //     setVisibilityEditProduct(true);
+    // };
 
-    const toggleVisibilityListProducts = () => {
-        setVisibilityListProducts(true);
-    };
+    // const toggleVisibilityListProducts = () => {
+    //     setVisibilityListProducts(true);
+    // };
 
-    const toggleVisibilityDetailProduct = () => {
-        setVisibilityDetailProduct(true);
-    };
+    // const toggleVisibilityDetailProduct = () => {
+    //     setVisibilityDetailProduct(true);
+    // };
 
-    const toggleVisibilityDeleteProduct = () => {
-        setVisibilityDeleteProduct(true);
-    };
+    // const toggleVisibilityDeleteProduct = () => {
+    //     setVisibilityDeleteProduct(true);
+    // };
 
-    const toggleVisibilityCreateCustomer = () => {
-        setVisibilityCreateCustomer(true);
-    };
+    // const toggleVisibilityCreateCustomer = () => {
+    //     setVisibilityCreateCustomer(true);
+    // };
 
-    const toggleVisibilityEditCustomer = () => {
-        setVisibilityEditCustomer(true);
-    };
+    // const toggleVisibilityEditCustomer = () => {
+    //     setVisibilityEditCustomer(true);
+    // };
 
-    const toggleVisibilityListCustomers = () => {
-        setVisibilityListCustomers(true);
-    }
+    // const toggleVisibilityListCustomers = () => {
+    //     setVisibilityListCustomers(true);
+    // }
 
-    const toggleVisibilityDetailCostumer = () => {
-        setVisibilityDetailCustomer(true)
-    }
+    // const toggleVisibilityDetailCostumer = () => {
+    //     setVisibilityDetailCustomer(true)
+    // }
 
     return (
         <div className='Funcional-div'>
             <ul className='Funcional-div-list'>
                 <li onClick={toggleVisibilityUpdateYourData}>▼  Alterar seu dados {visibilityUpdateYourData && <ProfileUpdate token={token} />}</li>
                 <li onClick={toggleVisibilityCatogryList}>▼  Listar Categorias {visibilityCatogryList && <CategoryList token={token} />}</li>
-                <li onClick={toggleVisibilityCreateProduct}>▼  Cadastrar Produto</li>
+                {/* <li onClick={toggleVisibilityCreateProduct}>▼  Cadastrar Produto</li>
                 <li onClick={toggleVisibilityEditProduct}>▼  Editar dados do produto</li>
                 <li onClick={toggleVisibilityListProducts}>▼  Listar Produtos</li>
                 <li onClick={toggleVisibilityDetailProduct}>▼  Detalhar Produto</li>
@@ -76,7 +76,7 @@ export default function Funcional() {
                 <li onClick={toggleVisibilityCreateCustomer}>▼  Cadastrar Cliente</li>
                 <li onClick={toggleVisibilityEditCustomer}>▼  Editar dados do cliente</li>
                 <li onClick={toggleVisibilityListCustomers}>▼  Listar Clientes</li>
-                <li onClick={toggleVisibilityDetailCostumer}>▼  Detalhar Cliente</li>
+                <li onClick={toggleVisibilityDetailCostumer}>▼  Detalhar Cliente</li> */}
             </ul>
         </div>
     );
